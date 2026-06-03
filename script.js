087c33f2999e0d7169a1df399fb94cbe1c8b923f
GOWTHAM9336 <gowthammani1162005@gmail.com>
2026-06-03 20:45:20 +0530

    update all

---
+++ script.js
@@ -0,0 +1,66 @@
+// Welcome Message
+function showMessage() {
+    alert("Thank You For Choosing Jayakumar Paal Pannai!");
+}
+
+// Page Loaded
+window.addEventListener("load", () => {
+    console.log("Jayakumar Paal Pannai Website Loaded Successfully");
+});
+
+// Login Form
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+
+    loginForm.addEventListener("submit", function(e) {
+
+        e.preventDefault();
+
+        const username = document.getElementById("username");
+        const password = document.getElementById("password");
+
+        if (!username || !password) {
+            alert("Username or Password field not found.");
+            return;
+        }
+
+        if (
+            username.value === "jayakumar" &&
+            password.value === "00-00-0000"
+        ) {
+
+            alert("Login Successful");
+            window.location.href = "index.html";
+
+        } else {
+
+            alert("Invalid Username or Password");
+
+        }
+    });
+}
+
+// Search Button
+const searchBtn = document.querySelector(".search-box button");
+
+if (searchBtn) {
+
+    searchBtn.addEventListener("click", () => {
+        alert("Searching Products...");
+    });
+
+}
+
+function logout(){
+
+    const confirmLogout = confirm(
+        "Are you sure you want to logout?"
+    );
+
+    if(confirmLogout){
+
+        window.location.href = "login.html";
+
+    }
+}
